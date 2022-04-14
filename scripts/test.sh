f57546ea18db355402459080973a8f775f13f8d5
77abood <75201045+77abood@users.noreply.github.com>
2022-04-14 07:20:00 +0300

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,12 +9,12 @@ echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
-set -e
+set -x
 # npm install --save-dev cross-env
 set +e
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
-set -e
+set -x
 npm test
