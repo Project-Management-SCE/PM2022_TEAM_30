49d42c4afb0b5736754c5f853f1c3d411efd97b1
Bhaa Alden Aldda <73523916+Bhaaldda@users.noreply.github.com>
2022-05-08 19:09:09 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("Success");
+// console.log("Success");
+// console.log("Success");
+const app = require('./app');
+
+	
+describe("Testing", () => {
+	test("Creating user" ,async(){
+		const response = await request(app).post("/User").send({style:'Home_user.css',firstnamex : "user1",lastnamex :" aa"});
+		exepct(response.statusCode).toEqual(200);
+	})
+})
