e637c3d8cf91fe4162afe66d76ddff962b79b69a
Bhaa Alden Aldda <73523916+Bhaaldda@users.noreply.github.com>
2022-05-08 19:33:22 +0300

    Update test.js
    Update unit test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 	
 describe("Testing", () => {
-	+npm test("Creating user",async()=> {
+	test("Creating user",async()=> {
 		const response = await request(app).post("/User").send({style:'Home_user.css',firstnamex : "user1",lastnamex :" aa"});
 		exepct(response.statusCode).toEqual(200);
 	})
