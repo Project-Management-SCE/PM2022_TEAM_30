28b81b07a50913b3607baf575cc570cd946c6662
mohammedabbbas <73596848+mohammedabbbas@users.noreply.github.com>
2022-05-10 00:26:11 +0300

    Update app.js
    database nonlocal

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require("path");
 const nodemailer = require("nodemailer");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/EVwaze');
+//mongoose.connect('mongodb://localhost:27017/EVwaze');
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.omcnp.mongodb.net/EVwaze?retryWrites=true&w=majority')
 var db=mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
