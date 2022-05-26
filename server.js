2a53e732b69df100d038f8207af6238f46482898
mohammedabbbas <73596848+mohammedabbbas@users.noreply.github.com>
2022-05-27 00:56:13 +0300

    initial deploy

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
  mongoose.connect(url);
 //mongoose.connect('mongodb+srv://admin:admin123@cluster0.omcnp.mongodb.net/EVwaze?retryWrites=true&w=majority');
 var app=require("./app.js");
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
- console.log(`Server has started on port ${port} ... `);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 //app.listen(3000,function(){
   //console.log("server is running at port 3000");
