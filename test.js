353b03686e089566528f2ae65d839718c49a1dc4
mohammd <mhmd.abood.2001@gmail.com>
2022-05-28 03:31:17 +0300

    Update test.js

--- test.js
+++ test.js
@@ -9,5 +9,62 @@ describe('Home User', function () {
       expect(res.header['content-type']).toBe('text/html; charset=utf-8');
       expect(res.statusCode).toBe(404);
     });
-    
+
+});
+describe('check routes admin/login', function () {
+
+    test('responds to /', async () => {
+      const res = await request(app).get('/admin/login');
+      expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(res.statusCode).toBe(200);
+    });
+
+});
+describe('check /', function () {
+
+    test('responds to /', async () => {
+      const res = await request(app).get('/');
+      expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(res.statusCode).toBe(200);
+    });
+
+});
+
+describe('check /user', function () {
+
+    test('responds to /', async () => {
+      const res = await request(app).get('/User');
+      expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(res.statusCode).toBe(200);
+    });
+
+});
+
+
+describe('check /Helper', function () {
+
+    test('responds to /', async () => {
+      const res = await request(app).get('/Helper');
+      expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(res.statusCode).toBe(500);
+    });
+
+});
+describe('check /Helpers', function () {
+
+    test('responds to /', async () => {
+      const res = await request(app).get('/helpers');
+      expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(res.statusCode).toBe(500);
+    });
+
+});
+describe('check /update Helpers', function () {
+
+    test('responds to /', async () => {
+      const res = await request(app).get('/UpdateHelper');
+      expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(res.statusCode).toBe(200);
+    });
+
 });
